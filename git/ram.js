ef6428ab4b383769eac08aad0ef787200d6b6e18
k.maheshkunta@gmail.com <k.maheshkunta@gmail.com>
2023-07-04 13:47:08 +0530

    first examples

--- git/ram.js
+++ git/ram.js
@@ -1,2 +1,13 @@
-console.log("mahesh");
-console.log('ram');
+//closures 
+function x(){
+    let a=7;
+  return   function y(){
+        let a=8;
+        console.log(a);
+    } 
+   //return y; are both same;
+}
+var z=x();
+console.log(z);
+//suppose thound of lines
+z();
